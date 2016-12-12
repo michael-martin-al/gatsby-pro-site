1caf8cdaa84536350f966d7f07717182eef71c24
Michael Martin <michael.martin@life.church>
2016-12-11 21:57:20 -0600

    cleaning up with .gitignore

---
+++ README.md
@@ -0,0 +1,3 @@
+# Gatsby Pro Site Starter
+
+More details coming soon.
--- node_modules/react-headroom/www/gatsby-config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-  siteMetadata: {
-    title: 'React Headroom',
-  },
-  linkPrefix: '/react-headroom',
-  sources: `${__dirname}/pages/`,
-}
-
-export default config
